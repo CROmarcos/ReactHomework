d87c6d0cfe4dc2c0b4c0ed78250713770a26efe5
CROmarcos <markopavlovic2010@hotmail.com>
2020-04-26 23:58:26 +0200

    Dodan searchbar za Events.

--- src/components/EventsMain/EventsMain.js
+++ src/components/EventsMain/EventsMain.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import InfoBox from '../InfoBox/InfoBox';
 import {Events, EventsSection} from './EventsMainStyle';
 import EventsIcon from '../../assets/img/event-icon.png';
 import SearchBar from '../SearchBar/SearchBar';
+import EventsArray from '../../lib/Events';
 
 const pageTitle='Događanja';
 
@@ -11,21 +12,32 @@ const name='How can we benefit from React Redux';
 const button='Prijavi se na predavanje';
 
 const EventsMain=()=>{
+    const [events, setEvents]=useState('');
+    const [selectedEvents, setSelectedEvents]=useState('');
+
+    useEffect(()=>{
+        const timer=setTimeout(()=>{
+            setEvents(EventsArray);
+            setSelectedEvents(EventsArray);
+        }, 1000);
+        return()=>clearTimeout(timer);
+    },[]);
+
+    const searchEvents=(name)=>{
+        const search=selectedEvents.filter(event=>event.title.toLowerCase().includes(name.toLowerCase()));
+        return setEvents(search);
+    }
+
     return(
         <main>
             <h1 class="PageTitle">{pageTitle}</h1>
             <EventsSection>
-                <SearchBar/>
+                <SearchBar backText="Search events" onValueChange={searchEvents}/>
             </EventsSection>
             <Events>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
-                <InfoBox url={EventsIcon} title={name} buttonText={button} hideEvent={false}/>
+                {events==='' ? '' : events.map((events,index)=>(
+                        <InfoBox key={index} url={EventsIcon} title={events.title} buttonText={button} hideEvent={false} />
+                ))}
             </Events>
         </main>
     );
